37fc5ef972531854d0f5893c4a19b7e3ee63e798
user-IVC <bagdik1990@mail.ru>
2024-09-07 10:57:06 +0300

    initial commit

--- app/src/main/java/com/example/mobiledeveloping/z2v3.kt
+++ app/src/main/java/com/example/mobiledeveloping/z2v3.kt
@@ -3,6 +3,7 @@ package com.example.mobiledeveloping
 fun z2v3() {
     println("Введите любое число")
     var Number = readln().toInt()
+    var Number1 = Number
     if (Number >= 2 && Number <= 5)
         {
         Number += 10
@@ -16,5 +17,14 @@ fun z2v3() {
     else {
         Number = 0
     }
-    println(Number)
+    when(Number1){
+        in 2..5 + 10 -> println("от 2 до 5")
+        in 7..40 - 100 -> println("от 7 до 40")
+        !in 0..3000 *3 -> println("меньше 0 и больше 3000")
+        else -> Number1 = 0
+    }
+    println("Результат IF:" + Number)
+    println("Результат When" + Number1)
+
+
 }
