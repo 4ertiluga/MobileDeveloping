fda52c7605d547f8527685c147c914fce6f08587
user-IVC <bagdik1990@mail.ru>
2024-09-14 09:23:44 +0300

    initial commit

--- app/src/main/java/com/example/mobiledeveloping/main.kt
+++ app/src/main/java/com/example/mobiledeveloping/main.kt
@@ -3,23 +3,25 @@ package com.example.mobiledeveloping
 fun main() {
     mainnn()
     println()
-    z1v3()
+    z1v3() //Бирюков
     println()
-    z1v7()
+    z1v7() //Сидаш
     println()
-    z2v3()
+    z2v3() //Бирюков
     println()
-    z2v4()
+    z2v4() //Сидаш
     println()
-    z3v3()
+    z3v3() //Бирюков
     println()
-    z3v22()
-    z4v3()
+    z3v22() //Сидаш
     println()
-    z4v9()
+    z4v3() //Бирюков
+    println()
+    z4v9() //Сидаш
     println()
     println()
 
+    //Общее задание #5
     val circle = Circle("Red", true, 5.0)
     val rectangle = Rectangle("Blue", false, 4.0, 6.0)
     val triangle = Triangle("Green", true, 3.0, 4.0, 5.0)
