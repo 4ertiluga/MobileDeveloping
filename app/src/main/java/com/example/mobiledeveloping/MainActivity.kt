4e2e066914a54270294fa194c5255e9dddff2706
dragomir <drkuleshov@yandex.ru>
2024-10-23 18:34:16 +0300

    Прогноз погоды 9, Передали данные на экран c API

--- app/src/main/java/com/example/mobiledeveloping/MainActivity.kt
+++ app/src/main/java/com/example/mobiledeveloping/MainActivity.kt
@@ -47,7 +47,20 @@ class MainActivity : ComponentActivity() {
                 val daysList = remember {
                     mutableStateOf(listOf<WeatherModel>())
                 }
-                getData("London", this, daysList)
+                val currentDay = remember {
+                    mutableStateOf(WeatherModel(
+                        "",
+                        "",
+                        "0.0",
+                        "",
+                        "",
+                        "0.0",
+                        "0.0",
+                        "",
+                    )
+                    )
+                }
+                getData("London", this, daysList, currentDay)
                 Image(
                     painter = painterResource(id = R.drawable.weather_bg),
                     contentDescription = "im1",
@@ -57,7 +70,7 @@ class MainActivity : ComponentActivity() {
                     contentScale = ContentScale.Crop
                 )
                 Column {
-                    MainCard()
+                    MainCard(currentDay)
                     TabLayout(daysList)
                 }
 
@@ -101,7 +114,9 @@ class MainActivity : ComponentActivity() {
 //
 //}
 
-private fun getData(city: String, context: Context, daysList: MutableState<List<WeatherModel>>){
+private fun getData(city: String, context: Context,
+                    daysList: MutableState<List<WeatherModel>>,
+                    currentDay: MutableState<WeatherModel>){
     val url = "https://api.weatherapi.com/v1/forecast.json?key=$API_KEY" +
             "&q=$city"+
             "&days=" +
@@ -114,6 +129,7 @@ private fun getData(city: String, context: Context, daysList: MutableState<List<
         {
             response ->
             val list = getWeatherByDays(response)
+            currentDay.value = list[0]
             daysList.value = list
         },
         {
